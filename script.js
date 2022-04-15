505a0f228e958f79b0de2460f64aeea28bc4025a
Francisco <montejof1998@gmail.com>
2022-04-15 10:56:09 -0700

    Now the user cant scroll in the menu

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const iconMenu = document.getElementById("iconMenu");
+const menu = document.getElementById("menuContainer");
+const closeIcon = document.getElementById("closeMenu");
 
+function noScroll() {
+  window.scrollTo(0.0);
+}
+
+const deployMenu = () => {
+  if (menu.classList.contains("off")) {
+    menu.classList.replace("off", "on");
+  }
+};
+
+const closeMenu = () => {
+  if (menu.classList.contains("on")) {
+    menu.classList.replace("on", "off");
+  }
+};
+
+const preventScroll = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  return false;
+};
+
+iconMenu.addEventListener("click", deployMenu);
+closeIcon.addEventListener("click", closeMenu);
+menu.addEventListener("wheel", preventScroll, { passive: false });
